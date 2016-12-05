131c389a25a2084165493c1ee2655981e6e9b917
Ivar Refsdal <ivar.refsdal@nsd.no>
2016-12-05 14:43:37 +0100

    ...

--- update.sh
+++ update.sh
@@ -7,7 +7,11 @@ git pull
 rm -f *.sqlite
 
 ./log_changes.py | tail -n1 >> country_rank.tsv
+cat country_rank.tsv | head -n1 > latest.tsv
+cat country_rank.tsv | tail -n1 >> latest.tsv
+
 git add country_rank.tsv
+git add latest.tsv
 git commit -m "update"
 git push
 
